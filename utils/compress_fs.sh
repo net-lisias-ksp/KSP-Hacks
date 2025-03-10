38ca43a6dd35f11a08ee2f6eb8345c7f934883d9
Lisias T <me@lisias.net>
2025-03-10 00:36:07 -0300

    Legal Mambo Jambo 2025 Edition

--- utils/compress_fs.sh
+++ utils/compress_fs.sh
@@ -3,25 +3,24 @@
 # This file is better visualized configuring a TAB to be 4 spaces width
 #
 #
-#    This file is part of KSP hacks (http://ksp.lisias.net/add-ons/KSP-Hacks)
-#    (C) 2018-21 Lisias T : http://lisias.net <support@lisias.net>
+#	This file is part of KSP-Hacks (http://ksp.lisias.net/add-ons/KSP-Hacks)
+#		© 2018-25 Lisias T : http://lisias.net <support@lisias.net>
 #
-#        KSPe API Extensions/L is double licensed, as follows:
+#	KSP-Hacks is double licensed, as follows:
+#		* SKL 1.0 : https://ksp.lisias.net/SKL-1_0.txt
+#		* GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
 #
-#        * SKL 1.0 : https://ksp.lisias.net/SKL-1_0.txt
-#        * GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+#	And you are allowed to choose the License that better suit your needs.
 #
-#        And you are allowed to choose the License that better suit your needs.
+#	KSP-Hacks is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
-#        KSPe API Extensions/L is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#	You should have received a copy of the SKL Standard License 1.0
+#	along with KSP-Hacks. If not, see <https://ksp.lisias.net/SKL-1_0.txt>.
 #
-#        You should have received a copy of the SKL Standard License 1.0
-#    along with KSPe API Extensions/L. If not, see <https://ksp.lisias.net/SKL-1_0.txt>.
-#
-#        You should have received a copy of the GNU General Public License 2.0
-#    along with KSPe API Extensions/L. If not, see <https://www.gnu.org/licenses/>.
+#	You should have received a copy of the GNU General Public License 2.0
+#	along with KSP-Hacks. If not, see <https://www.gnu.org/licenses/>.
 
 # You need afstool:
 # sudo port install afsctool
